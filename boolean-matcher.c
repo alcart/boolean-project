4d5946d4c7b849e24040d7ce957ce543c485819e
Guiseppe Martinez <martinezguiseppe@yahoo.com>
2019-04-17 19:48:22 -0400

    template for switch.

---
+++ boolean-matcher.c
@@ -0,0 +1,14 @@
+
+__global__ void gpu_sqrt(char* result, int index) {
+   
+  long element = (((long)blockIdx.x)*((long)blockDim.x) + ((long)threadIdx.x)); // maxes out at 2^42 - 1
+  long maxCores = 1 << 42;
+
+   switch(index){
+     case {IDX}:
+	for(long i = element; i < (1 << {VAR_COUNT}); i += maxCores) {          
+	  result = result && ({EXPR});
+        }
+	break;
+   }
+}
