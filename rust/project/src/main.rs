1a681c92befdb8d352dbf69d7aee0b9f763fd705
Victor M. Suarez <svmnotn@gmail.com>
2019-04-23 16:19:52 -0400

    add comments

--- rust/project/src/main.rs
+++ rust/project/src/main.rs
@@ -7,6 +7,7 @@ use truth_tester::{
     FnTester,
 };
 
+/// Test the given expression using the Parallel methods of the `FnTester`
 fn test_par<F: Fn(&S) -> bool + Send + Sync>(count: usize, expr: F) -> u128 {
     let tester = FnTester::new(count, &expr);
     let start = Instant::now();
@@ -17,6 +18,7 @@ fn test_par<F: Fn(&S) -> bool + Send + Sync>(count: usize, expr: F) -> u128 {
     start.elapsed().as_nanos()
 }
 
+/// Test the given expression using the Linear methods of the `FnTester`
 fn test_lin<F: Fn(&S) -> bool + Send + Sync>(count: usize, expr: F) -> u128 {
     let tester = FnTester::new(count, expr);
     let start = Instant::now();
@@ -28,12 +30,17 @@ fn test_lin<F: Fn(&S) -> bool + Send + Sync>(count: usize, expr: F) -> u128 {
 }
 
 fn main() {
+    // Create an internal type to save on typing.
     type EFn = Fn(&S) -> bool + Send + Sync;
 
+    // All expressions
     let fns: Vec<(usize, Box<EFn>)> = vec![include!("expr.rs.txt")];
 
+    // Go through each expression 10 times
     for _ in 0..10 {
         for (i, (size, func)) in fns.iter().enumerate() {
+            // if it has more than 45 variables quit,
+            // as it takes too long to run
             if *size >= 45 {
                 continue;
             }
@@ -41,7 +48,9 @@ fn main() {
             print!("PAR,{},{},", i + 1, size);
             let par = test_par(*size, &**func);
             println!("{}", par);
-
+            
+            // If it has more than 35 variables don't try to test
+            // linear times.
             if *size < 35 {
                 print!("LIN,{},{},", i + 1, size);
                 let lin = test_lin(*size, &**func);
